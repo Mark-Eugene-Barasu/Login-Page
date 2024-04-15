1050ba40cc2f9860f3dd7f248e5f0581f7e27b0d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-15 05:50:09 +0200

    Pushting up the new changes

---
+++ script.js
@@ -0,0 +1,18 @@
+// {
+
+//     let theForm = document.getElementById(`form`);
+//     // theForm.onmouseenter = theChanges();
+//     // theForm.onmouseout = undoChanges();
+    
+//     function theChanges(){
+//         theForm.style.backgroundColor = `blue`;
+//     }
+    
+    
+//     function undoChanges(){
+//         theForm.style.backgroundColor = `darkslategrey`;
+//     }
+    
+// }
+
+
